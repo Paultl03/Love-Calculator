2a47c66465a8900321822129053fe905680620ec
Paultl03 <Plogan39@gmail.com>
2019-08-02 07:36:33 -0700

    Initial Commit

---
+++ Love Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Love Calculator
+//
+//  Created by Paul Logan III on 8/1/19.
+//  Copyright © 2019 Paul Logan III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
